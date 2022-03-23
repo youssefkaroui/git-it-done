58e614ff5576d4e750302cb9f6a3d2a1fbb2de24
Youssef <youssefkaroui6@gmail.comm>
2022-03-23 12:50:15 -0400

    display-repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,39 @@
+var userFormEl=document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerel= document.querySelector("#repos-container");
+var repoSearchTerm= document.querySelector("#repo-search-term");
+
+
+var formSubmitHandler =function(event) {
+  event.preventDefault(); 
+  var username = nameInputEl.value.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+  console.log(event);
+}
+
+var displayRepos = function(repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
+};
+
+
 var getUserRepos = function(user) {
   // format the github api url
-  var apiUrl = ("https://api.github.com/users/" + user + "/repos");
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-  // make a request to the url
+  // make a get request to url
   fetch(apiUrl).then(function(response) {
+    console.log(response);
     response.json().then(function(data) {
-      console.log(data);
+      displayRepos(data, user);   
     });
   });
 };
-  
-getUserRepos();
+
+userFormEl.addEventListener("submit", formSubmitHandler);
