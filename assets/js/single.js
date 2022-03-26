80dfee630dfd26efb294a5be3326f85038716aa0
Youssef <youssefkaroui6@gmail.comm>
2022-03-26 13:59:18 -0400

    g

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,6 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
-var repoNameEl= document.querySelector("#repoName");
 
 var getRepoName = function() {
   // grab repo name from url query string
@@ -29,7 +28,7 @@ var getRepoIssues = function(repo) {
     if (response.ok) {
       response.json().then(function(data) {
         displayIssues(data);
-  
+
         // check if api has paginated issues
         if (response.headers.get("Link")) {
           displayWarning(repo);
@@ -40,6 +39,7 @@ var getRepoIssues = function(repo) {
       document.location.replace("./index.html");
     }
   });
+};
 
 var displayIssues = function(issues) {
   if (issues.length === 0) {
@@ -54,12 +54,11 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -69,14 +68,13 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
@@ -96,5 +94,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-
-getRepoName ();
+getRepoName();
